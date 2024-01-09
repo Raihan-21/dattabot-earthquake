45ed28c78916a864db7b35c8f5e1764ca078f50b
Muhammad Raihan <muhammadraihan118@gmail.com>
2024-01-09 21:14:48 +0700

    integrate google map api

---
+++ src/pages/alert.tsx
@@ -0,0 +1,144 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useCallback, useEffect, useState } from "react";
+import { axiosInstance } from "@/axios";
+
+import Datepicker from "react-datepicker";
+
+import {
+  Chart as ChartJS,
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
+import "react-datepicker/dist/react-datepicker.css";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const pieLabels = ["green", "yellow", "orange", "red"];
+
+export const getServerSideProps = async () => {
+  const responses = await Promise.all(
+    pieLabels.map((label) =>
+      axiosInstance.get(
+        `/query?format=geojson&alertlevel=${label}&starttime=2023-02-01`
+      )
+    )
+  );
+  const pieData = responses.map((res) => {
+    return res.data.metadata.count;
+  });
+
+  // const data = res.data;
+  return {
+    props: { data: pieData },
+  };
+};
+
+export default function Home({ data }: { data: any }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [pieData, setPieData] = useState<any>({
+    labels: pieLabels,
+    datasets: [
+      {
+        label: "Total occured",
+        data,
+        backgroundColor: pieLabels,
+      },
+    ],
+  });
+
+  const [startDate, setStartDate] = useState(
+    new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000)
+  );
+  const [endDate, setEndDate] = useState(new Date());
+  const [maxDate, setMaxDate] = useState(new Date());
+
+  const fetchData = useCallback(async () => {
+    if (!endDate) return;
+    setIsLoading(true);
+    try {
+      const responses = await Promise.all(
+        pieLabels.map((label) =>
+          axiosInstance.get(
+            `/query?format=geojson&alertlevel=${label}&starttime=${startDate
+              .toISOString()
+              .slice(0, 10)}&endtime=${endDate.toISOString().slice(0, 10)}`
+          )
+        )
+      );
+      setPieData((prevState: any) => ({
+        ...prevState,
+        datasets: [
+          {
+            ...prevState.datasets[0],
+            data: responses.map((res) => {
+              return res.data.metadata.count;
+            }),
+          },
+        ],
+      }));
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  }, [startDate, endDate, pieData]);
+  const selectDate = useCallback(
+    (dates: any) => {
+      const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
+    },
+    [maxDate]
+  );
+
+  const calculateMaxDate = useCallback(() => {
+    setMaxDate(new Date(startDate.getTime() + 30 * 24 * 60 * 60 * 1000));
+  }, [startDate]);
+
+  useEffect(() => {
+    calculateMaxDate();
+  }, [startDate]);
+  useEffect(() => {
+    fetchData();
+  }, [endDate]);
+
+  useEffect(() => {
+    console.log(pieData);
+  }, [pieData]);
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <Link href={"/"}>home</Link>
+      <p className="text-xl font-bold">Earthquake occured by alert level</p>
+      <Datepicker
+        selected={startDate}
+        startDate={startDate}
+        endDate={endDate}
+        maxDate={maxDate}
+        onChange={selectDate}
+        selectsRange
+        // inline
+        shouldCloseOnSelect
+      />
+      {isLoading ? <p>Loading...</p> : <Bar data={pieData} />}
+    </main>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,181 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Wrapper } from "@googlemaps/react-wrapper";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { axiosInstance } from "@/axios";
+
+import Datepicker from "react-datepicker";
+
+import {
+  Chart as ChartJS,
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
+import "react-datepicker/dist/react-datepicker.css";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import MapComponent from "@/components/organisms/MapComponent";
+import Link from "next/link";
+// import Marker from "@/components/organisms/Marker";
+import {
+  APIProvider,
+  Map,
+  Marker,
+  useApiIsLoaded,
+} from "@vis.gl/react-google-maps";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const pieLabels = ["green", "yellow", "orange", "red"];
 
 export const getServerSideProps = async () => {
-  const res = await Promise.all(
-    pieLabels.map(
-      (label) =>
-        `/query?format=geojson&alertlevel=${label}&starttime=2023-12-01`
-    )
-  );
-  console.log(res);
+  try {
+    const responses = await axiosInstance.get(
+      `/query?format=geojson&starttime=2023-12-01`
+    );
 
-  // const data = res.data;
-  return {
-    props: { data: "tes" },
-  };
+    const data = responses.data.features.map((data: any) => {
+      return { mag: data.properties.mag, geometry: data.geometry };
+    });
+    console.log(data);
+    // const data = res.data;
+    return {
+      props: {
+        data: data,
+      },
+    };
+  } catch (error: any) {
+    console.log(error.response);
+  }
 };
 
 export default function Home({ data }: { data: any }) {
-  const [pieData, setPieData] = useState<any>({
-    labels: pieLabels,
-    datasets: [
-      {
-        label: "",
-        data: [],
-      },
-    ],
-  });
+  const apiIsLoaded = useApiIsLoaded();
+
+  const [locationList, setLocationList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [startDate, setStartDate] = useState(
+    new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000)
+  );
+  const [endDate, setEndDate] = useState(new Date());
+  const [maxDate, setMaxDate] = useState(new Date());
+
+  const [markerData, setMarkerData] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    if (!endDate) return;
+    setIsLoading(true);
+    try {
+      const res = await axiosInstance.get(
+        `/query?format=geojson&starttime=${startDate
+          .toISOString()
+          .slice(0, 10)}&endtime=${endDate.toISOString().slice(0, 10)}`
+      );
+      setLocationList(
+        res.data.features.map((data: any) => {
+          return { mag: data.properties.mag, geometry: data.geometry };
+        })
+      );
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  }, [startDate, endDate]);
+  const selectDate = useCallback(
+    (dates: any) => {
+      const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
+    },
+    [maxDate]
+  );
+
+  const calculateMaxDate = useCallback(() => {
+    setMaxDate(new Date(startDate.getTime() + 30 * 24 * 60 * 60 * 1000));
+  }, [startDate]);
+
+  useEffect(() => {
+    calculateMaxDate();
+  }, [startDate]);
+  useEffect(() => {
+    fetchData();
+  }, [endDate]);
+
+  // useEffect(() => {
+  //   if (data.length)
+  //     setMarkerData(
+  //       data.map(
+  //         (position: any) =>
+  //           new window.google.maps.LatLng(
+  //             position.coordinates[0],
+  //             position.coordinates[1]
+  //           )
+  //       )
+  //     );
+  // }, [data]);
+
+  // useEffect(() => {
+  //   console.log(markerData);
+  // }, [markerData]);
+
+  const renderCircle = useCallback((magnitude: number) => {
+    return;
+  }, []);
+  useEffect(() => {
+    setLocationList(data);
+  }, [data]);
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    >
+      <Link href={"/alert"}>alert</Link>
+      <p className="text-xl font-bold">Earthquake occured by alert level</p>
+      <Datepicker
+        selected={startDate}
+        startDate={startDate}
+        endDate={endDate}
+        maxDate={maxDate}
+        onChange={selectDate}
+        selectsRange
+        // inline
+        shouldCloseOnSelect
+      />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="w-full max-w-[500px] h-[200px]">
+          <APIProvider apiKey={process.env.NEXT_PUBLIC_API_KEY!}>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <Map center={{ lat: 41.850033, lng: -87.6500523 }} zoom={2}>
+                {locationList.length &&
+                  locationList.map((location: any) => (
+                    <Marker
+                      position={{
+                        lat: location.geometry.coordinates[0],
+                        lng: location.geometry.coordinates[1],
+                      }}
+                    />
+                  ))}
+              </Map>
+            )}
+          </APIProvider>
+        </div>
+      )}
+    </main>
   );
 }
